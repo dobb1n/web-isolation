18c11d86fee9b4fa8543b1cb9c5f7abd188b8d00
dobb1n <dobbinwondermule@gmail.com>
2024-06-04 16:51:10 +0100

    updates

--- cloud-functions-start/functions/index.js
+++
@@ -1,9 +0,0 @@
-
-// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
-const functions = require('firebase-functions');
-// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
-const admin = require('firebase-admin');
-admin.initializeApp();
-// TODO(DEVELOPER): Write the blurOffensiveImages Function here.
-
-// TODO(DEVELOPER): Write the sendNotifications Function here.
--- functions/main.py
+++ functions/main.py
@@ -1,3 +1,9 @@
+from firebase_functions import firestore_fn, https_fn
+
+# The Firebase Admin SDK to access Cloud Firestore.
+from firebase_admin import initialize_app, firestore
+import google.cloud.firestore
+
 from firebase_admin import initialize_app
 from firebase_functions.firestore_fn import (
   on_document_created,
@@ -6,19 +12,14 @@ from firebase_functions.firestore_fn import (
 )
 
 initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
 
-@on_document_created(document="users/{userId}")
+@on_document_created(document="messages/{text}")
 def myfunction(event: Event[DocumentSnapshot]) -> None:
   # Get a dictionary representing the document
   # e.g. {'name': 'Marie', 'age': 66}
   new_value = event.data.to_dict()
 
   # Access a particular field as you would any dictionary
-  name = new_value["name"]
-
+  url = new_value["text"]
+  print(url)
   # Perform more operations ...
